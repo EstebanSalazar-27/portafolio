80648b1ee2c4cf1935b5497c9fd1efd5d234d94f
EstebanSalazar-27 <estebanenis27@gmail.com>
2022-07-08 13:24:24 -0300

    in this commit i have added a new skills cards in the section skills, and developed a news aos animation because i need that my progress bar trigger the animation just when the cards be visible in the viewport

--- src/sections/Skills/Skills.jsx
+++ src/sections/Skills/Skills.jsx
@@ -5,11 +5,16 @@ import "./skills.scss"
 import { withNamespaces } from 'react-i18next';
 import { Header } from '../../components/Header/Header'
 
+// Icons 
 import reactLogo from "../../images/skills/react.png"
 import jsLogo from "../../images/skills/js.png"
 import gitLogo from "../../images/skills/git-logo.png"
 import githubLogo from "../../images/skills/githubLogo.png"
-
+import materialLogo from "../../images/skills/material.png"
+import styledLogo from "../../images/skills/styledComponents.png"
+import i18nextLogo from "../../images/skills/i18next.png"
+import sassLogo from "../../images/skills/sass.png"
+import cssLogo from "../../images/skills/css-3.png"
 export const SkillsHeader = styled.h3`
     color: ${({ theme }) => theme.constrastHeaders};
     font-weight: 400;
@@ -56,13 +61,50 @@ export const Skills = (props) => {
                     data-aos-delay="100"
                     data-aos-once="true"
                 >
-                    <div className="achievement-box-icon">
+                    <div className="achievement-box-icon" >
                         <img src={reactLogo} alt="" />
                     </div>
                     <div className="achievement-box-namebar-area">
                         <span>ReactJs</span>
                         <div className="achievement-box-bar">
                             <div className="achievement-box-status-25" data-aos="skill-progress-50" data-aos-duration="1000" data-aos-once="true" data-aos-delay="1000">
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div className="achievement-box" data-aos="fade-up "
+                    data-aos-easing="ease-in-sine"
+                    data-aos-duration="800"
+                    data-aos-delay="100"
+                    data-aos-once="true"
+                >
+                    <div className="achievement-box-icon">
+                        <img src={materialLogo} alt="" />
+                    </div>
+                    <div className="achievement-box-namebar-area">
+                        <span>Material IU</span>
+                        <div className="achievement-box-bar">
+                            <div className="achievement-box-status-50 material-status" data-aos="skill-progress-50" data-aos-duration="1000" data-aos-once="true" data-aos-delay="1000">
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div className="achievement-box" data-aos="fade-up "
+                    data-aos-easing="ease-in-sine"
+                    data-aos-duration="800"
+                    data-aos-delay="100"
+                    data-aos-once="true"
+                >
+                    <div className="achievement-box-icon">
+                        <img src={styledLogo} alt="" />
+                    </div>
+                    <div className="achievement-box-namebar-area">
+                        <span>Styled Components</span>
+
+                        <div className="achievement-box-bar">
+                            <div className="achievement-box-status-75 styled-status" data-aos="skill-progress-50" data-aos-duration="1000" data-aos-once="true" data-aos-delay="1000">
                             </div>
                         </div>
                     </div>
@@ -85,14 +127,30 @@ export const Skills = (props) => {
                         </div>
                     </div>
                 </div>
-
+                <div className="achievement-box" data-aos="fade-up "
+                    data-aos-easing="ease-in-sine"
+                    data-aos-duration="800"
+                    data-aos-delay="100"
+                    data-aos-once="true"
+                >
+                    <div className="achievement-box-icon" >
+                        <img src={i18nextLogo} alt="" />
+                    </div>
+                    <div className="achievement-box-namebar-area">
+                        <span>I18next (Internationalization)</span>
+                        <div className="achievement-box-bar">
+                            <div className="achievement-box-status-25 i18next-status" data-aos="skill-progress-50" data-aos-duration="1000" data-aos-once="true" data-aos-delay="1000">
+                            </div>
+                        </div>
+                    </div>
+                </div>
                 <div className="achievement-box" data-aos="fade-up "
                     data-aos-easing="ease-in-sine"
                     data-aos-duration="800"
                     data-aos-delay="100"
                     data-aos-once="true">
                     <div className="achievement-box-icon">
-                        <span className='css-logo'>CSS</span>
+                        <img src={cssLogo} alt="" />
                     </div>
                     <div className="achievement-box-namebar-area">
                         <span>CSS</span>
@@ -109,12 +167,12 @@ export const Skills = (props) => {
                     data-aos-delay="100"
                     data-aos-once="true">
                     <div className="achievement-box-icon">
-                        <span className='sass-logo'>SASS</span>
+                        <img src={sassLogo} width={70} alt="gitLogo" />
                     </div>
                     <div className="achievement-box-namebar-area">
                         <span>SASS</span>
                         <div className="achievement-box-bar">
-                            <div className="achievement-box-status-100" data-aos="skill-progress-75" data-aos-easing="new-easing" data-aos-duration="1000" data-aos-once="true" data-aos-delay="1000">
+                            <div className="achievement-box-status-50 sass-status" data-aos="skill-progress-25" data-aos-easing="new-easing" data-aos-duration="1000" data-aos-once="true" data-aos-delay="1000">
                             </div>
                         </div>
                     </div>
